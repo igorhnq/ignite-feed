95ad116e414de04964f75b060f93457a1d3940fd
igorhnq <igorhenrique102@gmail.com>
2025-02-08 16:22:10 -0300

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Post } from "./Post"
+
+export function App() {
+    return (
+        <div>
+            <Post 
+            author="Igor Henrique"
+            content="mega do akon"     
+        />
+        <Post 
+            author="Ericao"
+            content="kkk"     
+        />
+        
+        </div>
+        
+    )
+}
+
+ 
