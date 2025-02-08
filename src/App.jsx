090d69921a94f13c31f56bb859888e75623eacfe
igorhnq <igorhenrique102@gmail.com>
2025-02-08 17:26:30 -0300

    feat: add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
+
+import './global.css'
 
 export function App() {
     return (
         <div>
+            <Header />
             <Post 
             author="Igor Henrique"
             content="mega do akon"     
