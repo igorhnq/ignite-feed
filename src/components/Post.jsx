188bafa3c7632f6d86e4201f86c8164e8132299e
igorhnq <igorhenrique102@gmail.com>
2025-02-11 22:38:19 -0300

    feat: add Post component

---
+++ src/components/Post.jsx
@@ -0,0 +1,42 @@
+import styles from './Post.module.css'
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/igorhnq.png" />
+                    <div className={styles.authorInfo}>
+                        <strong>Igor Henrique</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title='11 de Maio as 08:13' dateTime="2022-05-11 08:13:30">Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa</p>
+                <p>Acabei de subir mais um porjeto no meu portifa. É um projeto que eu fiz no NLW Return, evento da Rocketseat</p>
+                <p><a href="#">jane.design/doctorcare</a></p>
+                <p>
+                    <a href="#">#novoprojeto</a>{''}
+                    <a href="#">#nlw</a>{''}
+                    <a href="#">#rocketseat</a>
+                </p>
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea 
+                    placeholder='Deixe um comentário'
+                />
+
+                <footer>
+                    <button type='submit'>Publicar</button>
+                </footer>
+            </form>
+        </article>
+    )
+}
