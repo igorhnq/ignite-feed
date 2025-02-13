bcca49afac88286be58a14894d135a4fcb0abf99
igorhnq <igorhenrique102@gmail.com>
2025-02-12 23:30:19 -0300

    feat: add formatted data

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,21 @@
+import { format, formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
+
 import styles from './Post.module.css'
 
-export function Post({ author, publishedAt }) {
+export function Post({ author, publishedAt, content }) {
+    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'ás' HH:m'h'", {
+        locale: ptBR
+    })
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true
+    })
+
     return (
         <article className={styles.post}>
             <header>
@@ -14,18 +27,11 @@ export function Post({ author, publishedAt }) {
                     </div>
                 </div>
 
-                <time title='11 de Maio as 08:13' dateTime="2022-05-11 08:13:30"></time>
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
             </header>
 
             <div className={styles.content}>
-                <p>Fala galeraa</p>
-                <p>Acabei de subir mais um porjeto no meu portifa. É um projeto que eu fiz no NLW Return, evento da Rocketseat</p>
-                <p><a href="#">jane.design/doctorcare</a></p>
-                <p>
-                    <a href="#">#novoprojeto</a>{''}
-                    <a href="#">#nlw</a>{''}
-                    <a href="#">#rocketseat</a>
-                </p>
+                
             </div>
 
             <form className={styles.commentForm}>
