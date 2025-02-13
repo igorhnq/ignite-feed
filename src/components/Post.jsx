88a2fdf1d8ad1261ecc9f938b2b150dc7089b471
igorhnq <igorhenrique102@gmail.com>
2025-02-12 21:53:34 -0300

    feat: add Avatar component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
@@ -6,7 +7,7 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://github.com/igorhnq.png" />
+                    <Avatar src="https://github.com/ericopieri.png" />
                     <div className={styles.authorInfo}>
                         <strong>Igor Henrique</strong>
                         <span>Web Developer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
 
 import styles from './Sidebar.module.css'
 
@@ -11,7 +12,7 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/igorhnq.png" alt="" />
+                <Avatar src="https://github.com/igorhnq.png" />
 
                 <strong>Igor Henrique</strong>
                 <span>Web Developer</span>
