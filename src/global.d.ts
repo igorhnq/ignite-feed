e538df5bef72866ed523a1d913566e08937c0858
igorhnq <igorhenrique102@gmail.com>
2025-02-21 22:51:02 -0300

    chore: add svg module declaration

--- src/global.d.ts
+++ src/global.d.ts
@@ -12,4 +12,9 @@ declare module '*.module.sass' {
     const classes: { [key: string]: string };
     export default classes;
 }
+
+declare module "*.svg" {
+    const content: string;
+    export default content;
+  }
   
